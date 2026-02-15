e41cdefe9c1eb7cde80913492533d390bfa6a377
Gautam Agrawal <gautamagrawal542@gmail.com>
2026-02-15 22:28:01 +0530

    project-initiate

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello Word");
+let user = "gautam";
+console.log(user);
+//# sourceMappingURL=index.js.map
